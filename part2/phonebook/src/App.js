22543253aec3523dc57180fa392abc526c1628bf
Kevin <kevin.nguyen@engineering.ucla.edu>
2022-02-24 09:20:29 -0800

    Add filter and phone number fields

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,35 +2,60 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [filterName, setFilterName] = useState('')
+
+  const handleFilterName = (event) => {
+    setFilterName(event.target.value)
+  }
 
   const handleNameInput = (event) => {
     setNewName(event.target.value)
   }
 
+  const handleNumberInput = (event) => {
+    setNewNumber(event.target.value)
+  }
+
   const handleSubmit = (event) => {
     event.preventDefault()
-    setPersons([...persons, {name: newName}])
+
+    let inPhonebook = persons.find((person) => person.name === newName)
+    
+    if (inPhonebook === undefined) {
+      setPersons([...persons, {name: newName, number: newNumber}])
+    } else {
+      alert(newName + ' is already in the phonebook')
+    }
+    
     setNewName('')
+    setNewNumber('')
   }
 
   const displayNames = (persons) => {
-    return persons.map(person => <Person key={person.name} name={person.name} />)
+    let displayList = persons.filter(person => {
+      return person.name.toLowerCase().startsWith(filterName.toLowerCase(), 0)
+    }) 
+    
+    return displayList.map(person => <Person key={person.name} name={person.name} number={person.number} />)
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={handleSubmit} >
-        <div>
-          name: <input value={newName} onChange={handleNameInput} />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <Filter value={filterName} onChange={handleFilterName} />
+      <h2>Add Contact</h2>
+      <PersonForm nameValue={newName} 
+                  numberValue={newNumber} 
+                  handleNameInput={handleNameInput} 
+                  handleNumberInput={handleNumberInput} 
+                  handleSubmit={handleSubmit} />
       <h2>Numbers</h2>
       <table>
         <tbody>
@@ -41,9 +66,40 @@ const App = () => {
   )
 }
 
+const Filter = (props) => {
+  return (
+    <>
+    Search: <input value={props.value} onChange={props.onChange} />
+    </>
+  )
+}
+
+const PersonForm = (props) => {
+  return (
+    <form onSubmit={props.handleSubmit} >
+      <div>
+        name: <input value={props.nameValue} onChange={props.handleNameInput} />
+      </div>
+      <div>
+        number: <input value={props.numberValue} onChange={props.handleNumberInput} />
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  )
+}
+
 const Person = (props) => {
   return (
-    <tr>{props.name}</tr>
+    <tr>
+      <td>
+        {props.name}
+      </td>
+      <td>
+        {props.number}
+      </td>
+    </tr>
   )
 }
 
