8a8394547b62be1269c610dd34f4b5c4c1520d46
Kevin <kevin.nguyen@engineering.ucla.edu>
2022-02-23 18:34:55 -0800

    Initial commit for courseinfo part 2 assignment

---
+++ part2/courseinfo/src/App.js
@@ -0,0 +1,65 @@
+function App() {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return (
+    <Course course={course} />
+  );
+}
+
+const Course = ({course}) => {
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total exercises={course.parts} />
+    </div>
+  )
+}
+
+function Header(props) {
+  return <h1>{props.course}</h1>;
+}
+
+function Content(props) {
+  return (
+    <>
+      <Part part={props.parts[0].name} exercises={props.parts[0].exercises} />
+      <Part part={props.parts[1].name} exercises={props.parts[1].exercises} />
+      <Part part={props.parts[2].name} exercises={props.parts[2].exercises} />
+    </>
+  );
+}
+
+function Part(props) {
+  return <p>{props.part} {props.exercises}</p>;
+}
+
+function Total(props) {
+  return (
+    <p>Number of exercises {
+      props.exercises[0].exercises + props.exercises[1].exercises + props.exercises[2].exercises
+      }
+    </p>);
+}
+
+export default App;
