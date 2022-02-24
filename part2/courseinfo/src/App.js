594c67cba29233b046a64f202166b60c97a4e9e6
Kevin <kevin.nguyen@engineering.ucla.edu>
2022-02-23 20:36:32 -0800

    Move Course and child components into a seperate module, and import to App

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,3 +1,5 @@
+import Course from './components/Course'
+
 function App() {
   const courses = [
     {
@@ -58,46 +60,6 @@ function App() {
       {displayCourses(courses)}
     </div>
   );
-}
-
-const Course = ({course}) => {
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total exercises={course.parts} />
-    </div>
-  )
-}
-
-function Header(props) {
-  return <h2>{props.course}</h2>;
-}
-
-function Content(props) {
-  const displayPart = (parts) => {
-    let jsxPartList = parts.map(section => {
-      return <Part part={section.name} key={section.id} exercises={section.exercises} />
-    });
-
-    return jsxPartList
-  }
-
-  return (
-    <>
-      {displayPart(props.parts)}
-    </>
-  );
-}
-
-function Part(props) {
-  return <p>{props.part} {props.exercises}</p>;
-}
-
-function Total(props) {
-  let totalExercise = props.exercises.reduce((prev, part) => prev + part.exercises, 0)
-
-  return <p><strong>Number of exercises {totalExercise}</strong></p>;
 }
 
 export default App;
